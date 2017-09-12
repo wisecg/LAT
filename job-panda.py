d80a797f626afe30a01eb47b4e17c88c5dc13512
wisecg <wisecg.neontetra@gmail.com>
2017-09-12 08:28:48 -0700

    new calibration and slurm stuff

--- job-panda.py
+++ job-panda.py
@@ -31,7 +31,8 @@ pandaDir   = home+"/project/panda-skim"
 # waveDir = "/projecta/projectdirs/majorana/users/bxyzhu/waveskim"
 # latDir = "/projecta/projectdirs/majorana/users/bxyzhu/latskim"
 # pandaDir = "/projecta/projectdirs/majorana/users/bxyzhu/pandaskim"
-qsubStr = "qsub -l h_vmem=2G qsub-job.sh"
+# qsubStr = "qsub -l h_vmem=2G qsub-job.sh" # SGE mode
+qsubStr = "sbatch slurm-job.sh" # SLURM
 
 # =============================================================
 def main(argv):
