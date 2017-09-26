84d1bfd89aead0abdc15efdfab0b44f1bb44cde2
bxyzhu <arcmandak@ucla.edu>
2017-09-26 16:23:05 -0600

    Put in basic structure of lat3, will flesh out later when I'm not lazy

--- lat3.py
+++ lat3.py
@@ -12,7 +12,7 @@ v1: 07 Aug 2017
 
 ========= C. Wiseman (USC), B. Zhu (LANL) =========
 """
-import sys, time
+import sys, time, ROOT
 import numpy as np
 
 def main(argv):
@@ -22,48 +22,65 @@ def main(argv):
     startT = time.clock()
 
     dsNum, subNum = -1, -1
-    bAll, bbcMax, b = False, False
-    bCustom = False
+    calTree = ROOT.TChain("skimTree")
     customPar = ""
+    parList = []
 
     if len(argv) == 0:
         return
     for i, opt in enumerate(argv):
         if opt == "-all":
-            bAll = True
+            # Add all parameters
+            parList.append('bcMax')
             print "Tuning all cuts"
         if opt == "-bcMax":
-            bbcMax = True
+            parList.append('bcMax')
             print "Tuning bcMax"
         if opt == "-Custom":
-            bCustom = True
             customPar = str(argv[i+1])
+            parList.append(customPar)
             print "Tuning custom cut parameter: ", customPar
         if opt == "-s":
             dsNum, subNum = int(argv[i+1]), int(argv[i+2])
 
+    # Load calibration files
+    if dsNum == -1 or subNum == -1:
+        print "DS or subDS not set properly, exiting"
+        return
+    else:
+        #FIXME -- Add correct calibration list getter here
+        calList = GetCalList()
+        for i in calList: calTree.Add(i)
 
+    # Tune cuts
+    tunedPars = {}
+    for parName in parList:
+        tunedPars = TuneCut(dsNum, subNum, calTree, chList, parName, fastMode)
 
     stopT = time.clock()
     print "Stopped:",time.strftime('%X %x %Z'),"\nProcess time (min):",(stopT - startT)/60
 
 
-def TuneCut(dsNum, subNum, cal, chList, fastMode, cutName):
-    c = TCanvas("c","c",1600,600)
+def TuneCut(dsNum, subNum, cal, chList, parName, fastMode):
+    c = TCanvas("%s"%(parName),"%s"%(parName),1600,600)
     c.Divide(3,1,0.00001,0.00001)
     for ch in chList:
-
         eb, elo, ehi = 10,0,30
         e1dCut = 5.
-        vb, vlo, vhi = 1000,100,2000
-        d2Draw = "%s:trapENFCal>>b(%d,%d,%d,%d,%d,%d)"%(cutName,eb,elo,ehi,vb,vlo,vhi)
-        d2Cut = theCut + " && channel==%d" % ch
         d1Cut = theCut + " && trapENFCal > %d && trapENFCal < %d && channel==%d" % (e1dCut,ehi,ch)
-        outPlot = "./plots/%s/%s_ds%d_ch%d.png" % (cutName, cutName, dsNum,ch)
-        cut99,cut95,cut01,cut05,cut90 = MakeCutPlot(c,cal,var,eb,elo,ehi,vb,vlo,vhi,d2Draw,d2Cut,d1Cut,outPlot,fastMode)
-        
-        # print "DS-%d  Ch %d  bcMax99 %.2f  bcMax95 %.2f  bcMax90 %.2f" % (dsNum,ch,cut99,cut95,cut90)
+        d2Cut = theCut + " && channel==%d" % ch
+        nPass = skimTree.Draw("trapECalC:%s"%(parName), d1Cut, "goff")
+        nEnergy = skimTree.GetV1()
+        nCut = skimTree.GetV2()
+        nCutList = list(float(newOE[n]) for n in xrange(nPass))
+        nEnergyList = list(float(nEnergy[n] for n in xrange(nPass)))
+        vb, vlo, vhi = 5000, np.amin(nCutList), np.amax(nCutList)
 
+        d2Draw = "%s:trapENFCal>>b(%d,%d,%d,%d,%d,%d)"%(parName,eb,elo,ehi,vb,vlo,vhi)
+        outPlot = "./plots/%s/%s_ds%d_ch%d.png" % (parName, parName, dsNum,ch)
+        cut99,cut95,cut01,cut05,cut90 = MakeCutPlot(c,cal,var,eb,elo,ehi,vb,vlo,vhi,d2Draw,d2Cut,d1Cut,outPlot,fastMode)
+
+        return list(cut01,cut05,cut90,cut95,cut99)
 
 def MakeCutPlot(c,cal,var,eb,elo,ehi,vb,vlo,vhi,d2Draw,d2Cut,d1Cut,outPlot,fastMode):
     """ Repeated code is the DEVIL.  Even if you have to pass in 1,000,000 arguments. """
