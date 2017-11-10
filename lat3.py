75c0a5f5d7afce5241630b10cdc8ab82d50460e7
bxyzhu <arcmandak@ucla.edu>
2017-11-09 21:48:23 -0700

    Change riseNoise cut to have more significant figures

--- lat3.py
+++ lat3.py
@@ -388,7 +388,7 @@ def ApplyChannelCuts(dsNum):
                 else:
                     print "Channel %d doesn't exist, skipping"%(ch)
                     continue
-                    
+
                 print "    Writing to:",outFile
                 print "    Cut used:",chanCut,"\n"
 
@@ -421,9 +421,9 @@ def MakeCutDict(cInfo, dsNum, modNum, chList, calIdxLo, calIdxHi):
             # use an 'if' condition for zeros - those are bad cut records
             if fsVal > 0:
                 if ch in megaCut.keys():
-                    megaCut[ch] += "||(run>=%d && run<=%d && fitSlo<%.2f && riseNoise < (%.2f+%.2f*TMath::Log(1+TMath::Exp((trapENFCalC-%.2f)/%.2f))) )" % (runMin, runMax, fsVal, max(rnFD[ch][0], rnCD[ch][4]),rnFD[ch][1], rnFD[ch][2],rnFD[ch][3])
+                    megaCut[ch] += "||(run>=%d && run<=%d && fitSlo<%.2f && riseNoise < (%.3f+%.5f*TMath::Log(1+TMath::Exp((trapENFCalC-%.3f)/%.3f))) )" % (runMin, runMax, fsVal, max(rnFD[ch][0], rnCD[ch][4]),rnFD[ch][1], rnFD[ch][2],rnFD[ch][3])
                 else:
-                    megaCut[ch] = "((run>=%d && run<=%d && fitSlo<%.2f && riseNoise < (%.2f+%.2f*TMath::Log(1+TMath::Exp((trapENFCalC-%.2f)/%.2f))) )" % (runMin, runMax, fsVal, max(rnFD[ch][0], rnCD[ch][4]),rnFD[ch][1], rnFD[ch][2],rnFD[ch][3])
+                    megaCut[ch] = "((run>=%d && run<=%d && fitSlo<%.2f && riseNoise < (%.3f+%.5f*TMath::Log(1+TMath::Exp((trapENFCalC-%.3f)/%.3f))) )" % (runMin, runMax, fsVal, max(rnFD[ch][0], rnCD[ch][4]),rnFD[ch][1], rnFD[ch][2],rnFD[ch][3])
 
     for key in megaCut:
         megaCut[key] += ")"
