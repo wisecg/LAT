cdac1dc4c463a3f9563f3b7ecc57d4f9175e44a6
wisecg <wisecg.neontetra@gmail.com>
2018-02-02 09:57:05 -0800

    extpulser

--- sandbox/extPulser.py
+++ sandbox/extPulser.py
@@ -8,11 +8,39 @@ from matplotlib.colors import LogNorm
 import numpy as np
 import tinydb as db
 
+extPulserInfo = {
+        "7a": [[5942, 5945], [10,14,18,22], 200, 674]],
+        7:  [[5947, 5960], [10,14,18,22,26,30,34,38,42,46,50,50,54,58,62], 190, 674],
+        8:  [[5964, 5977], [10,14,18,22,26,30,34,38,42,46,50,50,54,58,62], 190, 624],
+        9:  [[5979, 5992], [10,14,18,22,26,30,34,38,42,46,50,50,54,58,62], 190, 624],
+        10: [[6191, 6204], [10,14,18,22,26,30,34,38,42,46,50,50,54,58,62], 190, 688],
+        11: [[6206, 6219], [10,14,18,22,26,30,34,38,42,46,50,50,54,58,62], 190, 608],
+        12: [[6934, 6944], [140,145,150,155,160,165,170,175,180,185,190], 18, 674],
+        13: [[6964, 6970], [4354,1257,1296,654,1278,1278,1278],0,[674,624,688,662,608,608,608]], # adc,att,chan
+        14: [[6971, 6976], [140,145,150,155,160,165,170,175,180,185,190], 18, 608],
+        15: [[6977, 6982], [140,145,150,155,160,165,170,175,180,185,190], 18, 624],
+        16: [[7002, 7007], [140,145,150,155,160,165,170,175,180,185,190], 18, 688],
+        17: [[7008, 7013], [140,145,150,155,160,165,170,175,180,185,190], 18, 662],
+        18: [[7219, 7233], [14,18,22,26,30,999,30,34,38,42,46,50,54,58,62], 155, 674],
+        19: [[7234, 7246], [14,18,22,26,30,34,38,42,46,50,54,58,62], 164, 624],
+        20: [[7247, 7259], [14,18,22,26,30,34,38,42,46,50,54,58,62], 146, 688],
+        21: [[7260, 7272], [14,18,22,26,30,34,38,42,46,50,54,58,62], 138, 662],
+        22: [[13168, 13181], [14,18,22,26,30,34,38,42,46,50,54,58,62], 999, 690]
+    # if run==7224 skip
+    }
+
 def main():
+
     # runTuneCut()
     # runByRun()
-    # getEfficiency1()
-    getEfficiency2()
+    # fitSloEfficiency1()
+    # fitSloEfficiency2()
+
+    riseTimeStudy()
+    # combineData()
+
+    # riseNoiseEfficiency()
+    # TEEfficiency()
 
 
 def runTuneCut():
@@ -271,7 +299,7 @@ def runByRun():
             pass
 
 
-def getEfficiency1():
+def fitSloEfficiency1():
     """ Plot the efficiency vs. energy """
     from ROOT import TChain, gROOT
     gROOT.ProcessLine("gErrorIgnoreLevel = 3001;")
@@ -337,7 +365,6 @@ def getEfficiency1():
         yEff.append(eff)
     yEff.reverse()
 
-
     # make a plot
     fig = plt.figure(figsize=(10,6),facecolor='w')
     p1 = plt.subplot(111)
@@ -364,70 +391,72 @@ def getEfficiency1():
     plt.savefig("../plots/efficiency_idx%d.pdf" % pIdx)
 
 
-def getEfficiency2():
-    """ Treat files individually instead of chaining them. """
+def fitSloEfficiency2():
+    """ Plot efficiency vs energy for the 3 lowest ext pulser sets. """
+
     from ROOT import TChain, gROOT
     gROOT.ProcessLine("gErrorIgnoreLevel = 3001;")
 
-    # want to fill these for each run
-    avgE = {} # atten vs. E (linearity)
-    effic = {} # efficiency vs E
+    pIdxs = [18,19,20]
 
-    # pIdx 20 settings, taken from ORCA logs (run database)
-    pIdx, extChan, syncChan = 20, 688, 672
-    riseTime = 146 # ns
-    attens = {7247:14, 7248:18, 7249:22, 7250:26, 7251:30, 7252:34, 7253:38, 7254:42, 7255:46, 7256:50, 7257:54, 7258:58, 7259:62}
+    for pIdx in pIdxs:
 
-    # get fitSlo value from the DB for this channel.
-    # 33: [[6904,6925],6904,7274],
-    # 34: [[7275,7279,7281,7295],7275,7614]
-    dsNum, modNum, calIdx = 0, 1, 33
-    calDB = db.TinyDB('../calDB.json')
-    pars = db.Query()
-    fsD = ds.getDBRecord("fitSlo_ds%d_idx%d_m%d_Peak" % (dsNum, calIdx, modNum), False, calDB, pars)
-    fsCut = fsD[extChan][2] # 90% value (used in LAT3)
+        avgE = {}
+        effic = {}
 
-    fig = plt.figure(figsize=(8,8),facecolor='w')
-    p1 = plt.subplot(111)
+        # pIdx 20 settings, taken from ORCA logs (run database)
+        pIdx, extChan, syncChan = 20, 688, 672
+        riseTime = 146 # ns
+        attens = {7247:14, 7248:18, 7249:22, 7250:26, 7251:30, 7252:34, 7253:38, 7254:42, 7255:46, 7256:50, 7257:54, 7258:58, 7259:62}
 
-    calInfo = ds.CalInfo()
-    runList = calInfo.GetSpecialRuns("extPulser",pIdx)
-    for run in runList:
+        # get fitSlo value from the DB for this channel.
+        dsNum, modNum, calIdx = 0, 1, 33
+        calDB = db.TinyDB('../calDB.json')
+        pars = db.Query()
+        fsD = ds.getDBRecord("fitSlo_ds%d_idx%d_m%d_Peak" % (dsNum, calIdx, modNum), False, calDB, pars)
+        fsCut = fsD[extChan][2] # 90% value (used in LAT3)
 
-        fileList = ds.getLATRunList([run],"%s/lat" % (ds.specialDir))
-        latChain = TChain("skimTree")
-        for f in fileList: latChain.Add("%s/lat/%s" % (ds.specialDir,f))
-        tNames = ["Entry$","channel","trapENFCal","fitSlo"]
-        theCut = "(channel==%d || channel==%d) && mH==2" % (syncChan, extChan)
-        tVals = wl.GetVX(latChain,tNames,theCut)
-        nPass = len(tVals["Entry$"])
-        if nPass == 0: continue
-        enfArr = [tVals["trapENFCal"][i] for i in range(nPass) if tVals["channel"][i]==extChan]
-        sloArr = [tVals["fitSlo"][i] for i in range(nPass) if tVals["channel"][i]==extChan]
-        enfArr, sloArr = np.asarray(enfArr), np.asarray(sloArr)
+        fig = plt.figure(figsize=(8,8),facecolor='w')
+        p1 = plt.subplot(111)
 
-        muE, stdE = np.mean(enfArr), np.std(enfArr)
-        muF, stdF = np.mean(sloArr), np.std(sloArr)
-        nTot = len(sloArr)
-        nAcc = len([fs for fs in sloArr if fs < fsCut])
-        eff = 100. * (nAcc/nTot)
+        calInfo = ds.CalInfo()
+        runList = calInfo.GetSpecialRuns("extPulser",pIdx)
+        for run in runList:
 
-        print("Run %d  Att %d  muE %-5.2f  stdE %-5.2f  muF %-5.2f  stdF %-5.2f  nTot %-5d  nAcc %-5d  eff %.8f" % (run,attens[run],muE,stdE,muF,stdF,nTot,nAcc,eff))
+            fileList = ds.getLATRunList([run],"%s/lat" % (ds.specialDir))
+            latChain = TChain("skimTree")
+            for f in fileList: latChain.Add("%s/lat/%s" % (ds.specialDir,f))
+            tNames = ["Entry$","channel","trapENFCal","fitSlo"]
+            theCut = "(channel==%d || channel==%d) && mH==2" % (syncChan, extChan)
+            tVals = wl.GetVX(latChain,tNames,theCut)
+            nPass = len(tVals["Entry$"])
+            if nPass == 0: continue
+            enfArr = [tVals["trapENFCal"][i] for i in range(nPass) if tVals["channel"][i]==extChan]
+            sloArr = [tVals["fitSlo"][i] for i in range(nPass) if tVals["channel"][i]==extChan]
+            enfArr, sloArr = np.asarray(enfArr), np.asarray(sloArr)
 
-        avgE[run] = muE
-        effic[run] = eff
+            muE, stdE = np.mean(enfArr), np.std(enfArr)
+            muF, stdF = np.mean(sloArr), np.std(sloArr)
+            nTot = len(sloArr)
+            nAcc = len([fs for fs in sloArr if fs < fsCut])
+            eff = 100. * (nAcc/nTot)
 
-        p1.cla()
-        xLo, xHi, bpX = muE - stdE*3, muE + stdE*3, 0.2
-        yLo, yHi, bpY = -10, 300, 0.1
-        nBinsY, nBinsX = int((yHi-yLo)/bpY), int((xHi-xLo)/bpX)
-        p1.hist2d(enfArr, sloArr, bins=[nBinsX,nBinsY], range=[[xLo,xHi],[yLo,yHi]], norm=LogNorm())
-        p1.axhline(fsCut, color='black', linewidth=3)
-        p1.set_xlabel("trapENFCal (keV)",horizontalalignment='right',x=1.0)
-        p1.set_ylabel("fitSlo",horizontalalignment='right',y=1.0)
-        p1.set_title("run %d  ch %d  fsCut %.2f  eff %.2f" % (run,extChan,fsCut,eff))
-        plt.tight_layout()
-        plt.savefig("../plots/efficiency_idx%d_run%d.pdf" % (pIdx, run))
+            print("Run %d  Att %d  muE %-5.2f  stdE %-5.2f  muF %-5.2f  stdF %-5.2f  nTot %-5d  nAcc %-5d  eff %.8f" % (run,attens[run],muE,stdE,muF,stdF,nTot,nAcc,eff))
+
+            avgE[run] = muE
+            effic[run] = eff
+
+            p1.cla()
+            xLo, xHi, bpX = muE - stdE*3, muE + stdE*3, 0.2
+            yLo, yHi, bpY = -10, 300, 0.1
+            nBinsY, nBinsX = int((yHi-yLo)/bpY), int((xHi-xLo)/bpX)
+            p1.hist2d(enfArr, sloArr, bins=[nBinsX,nBinsY], range=[[xLo,xHi],[yLo,yHi]], norm=LogNorm())
+            p1.axhline(fsCut, color='black', linewidth=3)
+            p1.set_xlabel("trapENFCal (keV)",horizontalalignment='right',x=1.0)
+            p1.set_ylabel("fitSlo",horizontalalignment='right',y=1.0)
+            p1.set_title("run %d  ch %d  fsCut %.2f  eff %.2f" % (run,extChan,fsCut,eff))
+            plt.tight_layout()
+            plt.savefig("../plots/efficiency_idx%d_run%d.pdf" % (pIdx, run))
 
     # make arrays for plotting
     yAtt, yEff, xEne = [], [], []
@@ -438,16 +467,11 @@ def getEfficiency2():
             xEne.append(avgE[run])
             print(run,attens[run],avgE[run],effic[run])
 
-    # fit energy vs attenuation
-    m, b = np.polyfit(xEne, yAtt, 1)
-    fit = np.polyfit(xEne, yAtt, 1)
-    fit_fn = np.poly1d(fit) # f(x) for the fit
-
     # make plot
     fig.clear()
     p1 = plt.subplot(211)
+
     p1.plot(xEne, yAtt, ".")
-    # p1.plot(xEne, fit_fn(xEne), '--k')
     p1.set_title("Att. vs Energy, m %.2f  b %.2f" % (m,b))
     p1.set_xlabel("Energy (keV)")
     p1.set_ylabel("Atten (db)")
@@ -461,6 +485,19 @@ def getEfficiency2():
 
     plt.tight_layout()
     plt.savefig("../plots/attVsE.pdf")
+
+
+def riseTimeStudy():
+    """ How much does fitSlo jump around? """
+    calInfo = ds.CalInfo()
+    runList = calInfo.GetSpecialRuns("extPulser",pIdx)
+    for run in runList:
+        fileList = ds.getLATRunList([run],"%s/lat" % (ds.specialDir))
+
+
+
+
+
 
 
 
