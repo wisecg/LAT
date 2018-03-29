d51588c36d4ab3347b7e4995934f89b374210202
bxyzhu <arcmandak@ucla.edu>
2018-03-29 14:18:41 -0600

    Updated DataSetInfo for DS6

--- sandbox/fill-thresh.py
+++ sandbox/fill-thresh.py
@@ -21,7 +21,7 @@ def main():
     dsNum = 5
     inDir = '/mnt/mjdDisk1/Majorana/data/sandbox/thresholds'
     threshDictList = generateThreshDict(dsNum, inDir)
-    print(len(threshDictList))
+    # print(len(threshDictList))
     print(sorted(threshDictList[0].keys()))
 
 
@@ -64,16 +64,17 @@ def findGood(threshDictList, dsNum, subDS, ch, searchStyle = 0):
 
 
 def generateThreshDict(dsNum = 1, inDir = '', fillDb = False):
-
     threshTree = ROOT.TChain("threshTree")
     # Note: for DS0, the original threshold generation had 76 subDS instead of 75, so include 1 more for DS0!
     for idx in range(ds.dsMap[dsNum]+1):
         threshTree.Add("{}/threshDS{}_{}.root".format(inDir, dsNum, idx))
 
     chList = ds.GetGoodChanList(dsNum)
-    if dsNum == 5: # remove 692 and 1232 (both beges, so who cares)
-        if 692 in chList: chList.remove(692)
-        if 1232 in chList: chList.remove(1232)
+
+    # NOTE (3/29/2018): In our first result, we excluded these channels from the threshold DB, will be including all channels this time around
+    # if dsNum == 5: # remove 692 and 1232 (both beges, so who cares)
+        # if 692 in chList: chList.remove(692)
+        # if 1232 in chList: chList.remove(1232)
 
     # Loop over threshold files
     keyList = []
@@ -101,7 +102,7 @@ def generateThreshDict(dsNum = 1, inDir = '', fillDb = False):
             threshDict[chan] = [threshTree.threshCal.at(iH), abs(threshTree.sigmaCal.at(iH))]
 
         # Print message if a channel is missing in the keys -- this means the channel was off
-        # The channel will NOT be included in the dictionary (we can change later)
+        # NOTE: The channel will NOT be included in the dictionary (we can change later if we want to artificially fill)
         for ch in chList:
             if ch not in threshDict.keys():
                 print ("Channel {} not in {}".format(ch, key))
@@ -112,39 +113,43 @@ def generateThreshDict(dsNum = 1, inDir = '', fillDb = False):
 
     # This way is slow but it's careful and we don't make any mistakes
     # Solve issues with missing parameters by looping through lists
+    # NOTE: If the parameters didn't exist in the dictionary to begin with, they won't be corrected or filled!
     for idx, tDict in enumerate(dictList):
         # Check if a value is 99999.0 or 999999.0 (typically bad calibration)
         for ch, values in tDict.items():
             # If the index is 0, can't fill with the previous index so must fill with the next
             if idx == 0 and (99999.0 in values or 999999.0 in values or values[0] < 0):
                 goodIdx = findGood(dictList, dsNum, idx, ch, 1)
-                print "Filling idx{:d} ch{:d}({:.3f},{:.3f}) with goodIDX{:d}:".format(idx, ch, values[0], values[1], goodIdx), dictList[goodIdx][ch]
+                print ("Filling idx{:d} ch{:d}({:.3f},{:.3f}) with goodIDX{:d}:".format(idx, ch, values[0], values[1], goodIdx), dictList[goodIdx][ch])
                 dictList[idx][ch] = dictList[goodIdx][ch]
             # If index is between 1 and the last index, we can use the alternating search method to find the closest good subDS
             elif idx < len(dictList)-1 and idx!= 0 and (99999.0 in values or 999999.0 in values or values[0] < 0):
                 goodIdx = findGood(dictList, dsNum, idx, ch, 0)
-                print "Filling idx{:d} ch{:d}({:.3f},{:.3f}) with goodIDX{:d}:".format(idx, ch, values[0], values[1], goodIdx), dictList[goodIdx][ch]
+                print ("Filling idx{:d} ch{:d}({:.3f},{:.3f}) with goodIDX{:d}:".format(idx, ch, values[0], values[1], goodIdx), dictList[goodIdx][ch])
                 dictList[idx][ch] = dictList[goodIdx][ch]
             # If index is the last one, must fill with previous
             elif idx == len(dictList)-1 and (99999.0 in values or 999999.0 in values or values[0] < 0):
                 goodIdx = findGood(dictList, dsNum, idx, ch, -1)
-                print "Filling idx{:d} ch{:d}({:.3f},{:.3f}) with goodIDX{:d}:".format(idx, ch, values[0], values[1], goodIdx), dictList[goodIdx][ch]
+                print ("Filling idx{:d} ch{:d}({:.3f},{:.3f}) with goodIDX{:d}:".format(idx, ch, values[0], values[1], goodIdx),  dictList[goodIdx][ch])
                 dictList[idx][ch] = dictList[goodIdx][ch]
 
     # Final check
-    print "Final Checks"
+    print ("Performing Final Checks")
+    numErr = 0
     for idx, tDict in enumerate(dictList):
         for ch, values in tDict.items():
             if 99999.0 in values or 999999.0 in values:
-                print "Warning, bad fit found:", idx, ch
+                print ("Warning, bad fit found:", idx, ch)
                 print dictList[idx][ch]
+                numErr += 1
             if values[0] < 0:
-                print "Warning, negative value found:", idx, ch, values
-
+                print ("Warning, negative value found:", idx, ch, values)
+                numErr += 1
         # Finally fill DB here if all values look good
         if fillDb:
             wl.setDBRecord({"key":keyList[idx],"vals":dictList[idx]}, forceUpdate=False)
             wl.setDBRecord({"key":keyList[idx],"vals":dictList[idx]}, forceUpdate=True)
+    print("Finished final checks, found {} errors".format(numErr))
     return dictList
 
 
