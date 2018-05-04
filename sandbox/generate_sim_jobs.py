85a944cba4c9f90c4fa6a0d2bf943c7a7caa6f81
bxyzhu <arcmandak@ucla.edu>
2018-05-04 15:08:49 -0600

    Should be able to generate surface alpha/nuclear recoil jobs now

--- sandbox/generate_sim_jobs.py
+++ sandbox/generate_sim_jobs.py
@@ -19,14 +19,15 @@ def main():
 
     # Calibration Jobs
     # genCalMacros(nStart=601, nStop=999, mod=1, macroDir=macroDir, outDir=outDir, nEvents = 500000)
-    # runAllCalJobs(mod=1, nStart=601, nStop = 999, macroDir=macroDir)
 
     # Surface Alpha Jobs
     # Surface Sources: DUPTFE, DUCopper
-    genSurfaceMacros(nStart=1, nStop=2, mod=1, macroDir=macroDir, outDir=outDir)
-    # genAlphaMacros(nStart=1, nStop=999, mod=1, macroDir=macroDir, outDir=outDir, nEvents = 500000)
+    # genSurfaceMacros(sourceName='DUPTFE', nStart=1, nStop=1, macroDir=macroDir, outDir=outDir)
+    # genAlphaMacros(sourceName='DUPTFE', nStart=1, nStop=1, macroDir=macroDir, outDir=outDir)
 
-def genCalMacros(mod=1, nStart=1, nStop=100, oldAF = False, macroDir='.', outDir='/mnt/mjdDisk1/Majorana/users/psz/MAGE/Simulations/5M', nEvents = 500000):
+    # runAllJobs(sourceName='Cal', mod=1, nStart=601, nStop = 999, macroDir=macroDir)
+
+def genCalMacros(mod=1, nStart=1, nStop=100, oldAF = False, macroDir='.', outDir='.', nEvents = 500000):
     for nFile in range(nStart, nStop+1):
         f = open('{}/M{}CalSource_A224_Z88_p{:03d}.mac'.format(macroDir, mod, nFile), 'w+')
 
@@ -59,13 +60,12 @@ def genCalMacros(mod=1, nStart=1, nStop=100, oldAF = False, macroDir='.', outDir
         f.write("/MG/generator/MJDCalibration/setZ 88\n")
         f.write("/grdm/nucleusLimits 208 224 81 88\n")
         f.write("/run/beamOn {}".format(nEvents))
-
         f.close()
 
 
-def genSurfaceMacros(sourceName='DUPTFE', mod=1, nStart=1, nStop=100, macroDir='.', outDir='/mnt/mjdDisk1/Majorana/users/psz/MAGE/Simulations/5M', nEvents = 5000000):
+def genSurfaceMacros(sourceName='DUPTFE', nStart=1, nStop=100, macroDir='.', outDir='.', nEvents = 5000000):
     for nFile in range(nStart, nStop+1):
-        f = open('{}/M{}Alpha_A210_Z84_gss_p{:03d}.mac'.format(macroDir, mod, nFile), 'w+')
+        f = open('{}/MJDem_{}_A210_Z84_gss_p{:03d}.mac'.format(macroDir, sourceName, nFile), 'w+')
 
         f.write("/MG/manager/mglog routine\n")
         f.write("/MG/eventaction/reportingfrequency 100000\n")
@@ -81,7 +81,7 @@ def genSurfaceMacros(sourceName='DUPTFE', mod=1, nStart=1, nStop=100, macroDir='
         f.write("/run/initialize\n")
         f.write("/MG/generator/select GSS\n")
         f.write("/MG/eventaction/rootschema GSS\n")
-        f.write("/MG/eventaction/rootfilename {}/M{}Alpha_A210_Z84_gss_p{:03d}.root\n".format(outDir, mod, nFile))
+        f.write("/MG/eventaction/rootfilename {}/MJDem_{}_A210_Z84_gss_p{:03d}.root\n".format(outDir, sourceName, nFile))
 
         # Add Volumes here
         for vol in volList:
@@ -109,8 +109,54 @@ def genSurfaceMacros(sourceName='DUPTFE', mod=1, nStart=1, nStop=100, macroDir='
         f.close()
 
 
-def runAllCalJobs(mod=1, nStart=0, nStop=None, macroDir='.'):
-    fileList = sorted(glob.glob('{}/M{}CalSource_*'.format(macroDir, mod)))
+def genAlphaMacros(sourceName='DUPTFE', nStart=1, nStop=100, macroDir='.', outDir='.'):
+    # This assumes that the output directory of the gss files is the same as the output directory of the surf files
+    # Requires pyROOT!
+    import ROOT
+    for nFile in range(nStart, nStop+1):
+        nEvents = 0
+        f = open('{}/MJDem_{}_A210_Z84_surf_p{:03d}.mac'.format(macroDir, sourceName, nFile), 'w+')
+        f.write("/MG/manager/mglog routine\n")
+        f.write("/MG/eventaction/reportingfrequency 10000\n")
+        f.write("/MG/manager/seedWithUUID\n")
+        f.write("/MG/processes/realm BBdecay\n")
+        f.write("/MG/processes/lowenergy true\n")
+        f.write("/MG/processes/useAllHP true\n")
+        f.write("/MG/geometry/detector MJDemonstrator\n")
+        f.write("/MG/geometry/WorldMaterial Vacuum\n")
+        f.write("/MG/eventaction/rootschema MCRun\n")
+        f.write("/MG/eventaction/rootfilename {}/MJDem_{}_A210_Z84_surf_p{:03d}.root\n".format(outDir, sourceName, nFile))
+        f.write("/MG/io/MCRun/SetSensitiveIDLabelScheme askGeom\n")
+        f.write("/MG/io/MCRun/setRunID {:03d}\n".format(nFile))
+        f.write("/MG/io/MCRun/useTimeWindow true\n")
+        f.write("/MG/io/MCRun/setTimeWindow 86400 second\n")
+        f.write("/run/initialize\n")
+        f.write("/MG/demonstrator/muonVetoOn false\n")
+        f.write("/MG/demonstrator/innerCopperOn true\n")
+        f.write("/MG/generator/select RDMiso\n")
+        f.write("/gun/energy 0 eV\n")
+        f.write("/grdm/ion 210 84 0\n")
+        f.write("/grdm/nucleusLimits 206 210 80 84\n")
+
+        # Here we have to find the gss ROOT file and get the number of entries to set for the number of events
+        gssFileName = '{}/MJDem_{}_A210_Z84_gss_p{:03d}.root'.format(outDir, sourceName, nFile)
+        gssFile = ROOT.TFile(gssFileName)
+        tree = gss_file.Get('GSSTree')
+        nEvents = tree.GetEntries()
+        gssFile.Close()
+
+        f.write("/MG/generator/gsspositionsfile {}\n".format(gssFileName))
+        f.write("/run/beamOn {}".format(nEvents))
+        f.close()
+
+def runAllJobs(sourceName='Cal', macroTye='gss', mod=1, nStart=0, nStop=None, macroDir='.'):
+    # Macro type is either gss or surf
+    fileList = []
+    if sourceName == 'Cal':
+        fileList = sorted(glob.glob('{}/M{}CalSource_*.mac'.format(macroDir, mod)))
+    else:
+        fileList = sorted(glob.glob('{}/MJDem_{}_A210_Z84_{}_*.mac'.format(macroDir, sourceName, macroType)))
+
     if nStop > len(fileList):
         print("Stop value too large")
         return
