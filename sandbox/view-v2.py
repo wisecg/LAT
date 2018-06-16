495dbbdf6b602f0e57c51f5822ffe214b70451f8
Clint Wiseman <wisecg.neontetra@gmail.com>
2018-06-16 19:13:53 -0400

    print retrigger waveform

--- sandbox/view-v2.py
+++ sandbox/view-v2.py
@@ -18,9 +18,9 @@ def main(argv):
             quickDraw = True
 
     ds = "5A"
-    # tt = TChain("skimTree")
+    tt = TChain("skimTree")
     # tt.Add("~/project/cal/lat/*.root")
-    # tt.Add("~/project/cal/lat/latSkimDS1_run13774_0.root")
+    tt.Add("~/project/cal/lat/latSkimDS1_run13774_0.root")
 
     # tCut = "trapENFCal > 238 && trapENFCal < 239" # 238 kev wf, thesis plot
     # tCut = "trapENFCal >= 1.0 && trapENFCal < 1.2 && channel!=598  && trapENFCal > threshKeV+3*threshSigma && fitSlo < 100" # 1 kev wf, thesis plot
@@ -29,10 +29,12 @@ def main(argv):
     # tCut = "trapENFCal > 16 && trapENFCal < 16.1 && fitSlo > 100"
 
     # don't open the final files as tchains, it made a big segfault??
-    tf = TFile("~/project/bkg/cut/final/final_DS%s.root" % ds)
-    tt = tf.Get("skimTree")
+    # tf = TFile("~/project/bkg/cut/final/final_DS%s.root" % ds)
+    # tt = tf.Get("skimTree")
 
-    tCut = "trapENFCal >= 1 && trapENFCal < 4"
+    # tCut = "trapENFCal >= 1 && trapENFCal < 4"
+
+    tCut = "trapENFCal < 1.5 && tOffset > 1000" # try to grab a retrigger waveform
 
     n = tt.Draw("Entry$:Iteration$",tCut,"goff")
     evt, itr = tt.GetV1(), tt.GetV2()
@@ -102,14 +104,16 @@ def main(argv):
         if diff > 0: waveDenoised = waveDenoised[diff:]
 
         plt.cla()
-        plt.plot(waveTS, waveBLSub, 'b', alpha=0.2, label='Raw WF, %.2f keV' % (hitE))
+        plt.plot(waveTS, waveBLSub, 'b', lw=2, label='Raw WF, %.2f keV' % (hitE))
+        # plt.plot(waveTS, waveBLSub, 'b', alpha=0.2, label='Raw WF, %.2f keV' % (hitE))
         # plt.plot(waveTS, waveLP, 'r', alpha=0.7, label='Low-pass filter')
-        plt.plot(waveTS, waveDenoised, "r", label="Denoised WF")
+        plt.plot(waveTS, waveDenoised, "r", lw=2, alpha=0.5, label="Denoised WF")
 
 
         plt.xlabel("Time (ns)", ha='right', x=1)
         plt.ylabel("Voltage (ADC)", ha='right',y=1)
         plt.legend(loc=4)
+        plt.tight_layout()
 
         plt.pause(0.0001) # scan speed, does plt.show(block=False) automatically
 
