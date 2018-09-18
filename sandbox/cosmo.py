b05c460c1f138150ac2c00167ea68a12a4c100e1
Clint Wiseman <wisecg.neontetra@gmail.com>
2018-09-18 14:51:11 -0700

    cosmo calc weight

--- sandbox/cosmo.py
+++ sandbox/cosmo.py
@@ -72,12 +72,18 @@ def getCurves():
 
     dsList = [1,2,3,4,50,51,52,6] # 50=5A, etc.
 
+    df = pd.read_hdf("../data/lat-runTimes.h5")
+    df = df.loc[ df['ds'].isin(dsList) ]
+    t0 = df.loc[ df['ds'] == dsList[0] ]['start'].iloc[0]
+
     detIDs = det.allDetIDs
     detAMs = det.allActiveMasses
     detList = sorted([int(cpd) for cpd in det.allDetIDs])
+
     enrList = [cpd for cpd in detList if det.isEnr(cpd)]
     enrDets = set(df.loc[ df['det'].isin(enrList) & df['ds'].isin(dsList) ]['det'])
     enrMass = sum([ detAMs[detIDs[str(cpd)]] for cpd in enrDets])
+    enrExp = df.loc[ df['det'].isin(enrList) & df['ds'].isin(dsList) ]["expo"].sum()
 
     t12 = {"68Ge": 0.7414}
 
@@ -87,10 +93,6 @@ def getCurves():
     dfFit.loc["M1"] = [1817.44, 97.2, 57.89, 10.1, 6.95, 10.9, 11.01, 12.0, 38.13, 13.5, 47.31, 16.3, 0.00, 19.9, 1709.10, 49.0]
     dfFit.loc["M2"] = [302.47, 40.7, 27.32, 6.3, 59.00, 9.3, 0.0, 3.3, 3.28, 5.5, 0.0, 7.0, 12.34, 7.6, 417.77, 24.0]
 
-    df = pd.read_hdf("../data/lat-runTimes.h5")
-    df = df.loc[ df['ds'].isin(dsList) ]
-    t0 = df.loc[ df['ds'] == dsList[0] ]['start'].iloc[0]
-
     for iso in t12:
 
         fSum = 0
@@ -99,13 +101,19 @@ def getCurves():
 
             dfCPD = df.loc[ df['det'] == cpd, ['start','stop','lt']]
             if dfCPD.shape[0] == 0: continue
+            aMass = detAMs[detIDs[str(cpd)]]
 
-            # weighting factor: det active mass / total active mass
-            wt = detAMs[detIDs[str(cpd)]] / enrMass
+            # weighting factor 1: det active mass / total active mass
+            wt1 = detAMs[detIDs[str(cpd)]] / enrMass
+
+            # weighting factor 2: det exposure / total exposure
+            expo = dfCPD['lt'].sum()/86400 * aMass/1000
+            wt = expo / enrExp
+            expTot += expo
 
             # calculate Fij terms
             eFac = dfCPD.apply(Fij, axis=1, args=(t12[iso], t0))
-            fTot = eFac.sum() / wt
+            fTot = eFac.sum() * wt
             fSum += fTot
 
             # sanity check stuff
@@ -116,11 +124,12 @@ def getCurves():
             rt = (dfCPD['stop'] - dfCPD['start']).sum()
             rtCal = tf - ts
 
-            print("%d  fTot %.3f  f0 %.3f  lt/rt %.3f, rt/rtCal %.2f %%" % (cpd, fTot, f0, lt/rt, 100 * rt/rtCal))
+            print("%d  fTot %.3f  f0 %.3f  lt/rt %.3f  rt/rtCal %.2f%%  wt1 %.3f  wt2 %.3f  expo %.3f" % (cpd, fTot, f0, lt/rt, 100 * rt/rtCal, wt1, wt, expo))
 
         N_c = dfFit[iso].sum() # this is all detectors, fit result
         N_0 = N_c / fSum
 
+        print("expo (loop) %.2f, expo (df) %.2f" % (expTot, enrExp))
         print(iso, "N_c: %.1f, N_0: %.2f" % (N_c, N_0))
 
         # do we need some kinda normalization term?
